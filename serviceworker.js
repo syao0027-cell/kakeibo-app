46256359cd3b50c2217d370cc3978c7e26581214
syao0027-cell <sya.o0027@gmail.com>
2026-06-15 00:39:16 +0900

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'kakeibo-app-v0.9'; // v0.8 から v0.9 に変更
+const ASSETS = [
+  './',
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json',
+  'icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
